28fe02aadea635d9025dbe6feb96ee4fbb0e02d9
fran_devv <fran_q98@hotmail.com>
2021-02-26 12:28:35 -0300

    More sections
    Add section: contact
    Add section: footer

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
-$(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 1080);
-});
+var n = localStorage.getItem('on_load_counter');
+
+if (n === null) {
+  n = 0;
+}
+n++;
+
+localStorage.setItem("on_load_counter", n);
+
+nums = n.toString().split('').map(Number);
+document.getElementById('count').innerHTML = '';
+for (var i of nums) {
+  document.getElementById('count').innerHTML += '<span class="counter-item">' + i + '</span>';
+}
